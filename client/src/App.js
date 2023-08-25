df2f819e125ad6f21f348879855276b722bacb61
Hyeun9991 <hyeun9991@gmail.com>
2023-08-25 17:14:11 +0900

    redux & tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { changeCount } from './store/actions/countActions';
+
 function App() {
-  return <div className="App">App</div>;
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type));
+  };
+
+  return (
+    <div>
+      <p>{count.number}</p>
+      <div>
+        <button onClick={() => countChangeHandler('DECREASE')}>감소</button>
+        <button onClick={() => countChangeHandler('INCREASE')}>증가</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
